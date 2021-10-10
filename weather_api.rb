8113a2249eb9aabd7796d233a348c1c94033a084
Kent <kentallananderson@gmail.com>
2021-10-09 21:12:40 -0500

    Late commit

--- weather_api.rb
+++ weather_api.rb
@@ -1,5 +1,23 @@
+
+puts "Enter a city:"
+city = gets.chomp
+
+puts "Enter weather Celcius or Fahrenheit:"
+units = gets.chomp
+if units == "Celcius"
+  units = "metric"
+elsif units == "Fahrenheit"
+  units = "imperial"
+else
+  puts "Enter Fahrenheit or Celcius"
+end 
+
 require 'http'
 
-response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=metric")
+response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=#{units}")
 
-pp response.parse(:json)["main"]["temp"]
+puts "In #{city}"
+puts "The lowest temp is #{response.parse(:json)["main"]["temp_min"]}"
+puts "The highest temp is #{response.parse(:json)["main"]["temp_max"]}"
+puts "The humidity is #{response.parse(:json)["main"]["humidity"]}"
+puts "The windspeed is #{response.parse(:json)["wind"]["speed"]}"
